353f723262418d8807f147d8b668203efba62a12
Malcolm Idlebird <malcolmidlebird@Malcolms-MacBook-Pro.local>
2021-02-15 17:41:01 -0700

    completed markdown functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,106 @@
+const date = require("date-and-time");
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license ="MIT") {
+    licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  }
+  else if (license ="The Unlicense") {
+    licenseBadge = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+  }
+  else {
+    licenseBadge = ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  console.log(licenseBadge);
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license, user) {
+  if(license = "MIT"){
+    const now = new Date();
+    currentYear = date.format(now, "YYYY");
+    licenseText =`Copyright ${currentYear} ${user}
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+  }
+  else if(license = "The Unlicense") {
+    licenseText =`This is free and unencumbered software released into the public domain.
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
+    In jurisdictions that recognize copyright laws, the author or authors
+    of this software dedicate any and all copyright interest in the
+    software to the public domain. We make this dedication for the benefit
+    of the public at large and to the detriment of our heirs and
+    successors. We intend this dedication to be an overt act of
+    relinquishment in perpetuity of all present and future rights to this
+    software under copyright law.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+    For more information, please refer to <http://unlicense.org/>`;
+  }
+  else {
+    licenseText=''
+  }
+  return licenceText;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  //deconstruntion
+  const {userName , userEmail , repoName , projectTitle , projectDescription , installNotes , usageNotes , contribNotes, licenseSelection , testing } = data ;
+  
+  //badge links
+  const licenseBadge = renderLicenseBadge(licenseSelection);
+  const licenseText = renderLicenseSection(licenseSelection, userName);
+  return `${licenseBadge}
+  # ${data.projectTitle}
+  
+  ## Description
+  ${data.projectDescription}
 
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [ContactUs](#ContactUs)
+  
+  ## Installation
+  ${data.installNotes}
+
+  ## Usage
+  ${data.usageNotes}
+
+  ## License
+  ${licenseText}
+
+  ## Contributions
+  ${data.contribNotes}
+
+  ## Tests
+  ${data.testing}
+
+  ## ContactUs
+  [Email](mailto://${data.userEmail})
+  [GitHub](http://github.com/${data.userName})
+ 
 `;
 }
 
